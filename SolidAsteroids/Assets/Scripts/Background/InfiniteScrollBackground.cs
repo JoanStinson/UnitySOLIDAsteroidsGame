b07e2fcaabe28f404718e46c710d285a55806e13
Joan Ginard Mateo <jgm7.hd@gmail.com>
2021-12-10 19:14:45 +0100

    Background and Prefabs

---
+++ SolidAsteroids/Assets/Scripts/Background/InfiniteScrollBackground.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class InfiniteScrollBackground : MonoBehaviour
+{
+    [SerializeField]
+    private float _scrollSpeed = -100f;
+
+    private float _rightEdge;
+    private float _leftEdge;
+
+    private void Start()
+    {
+        float width = GetComponent<RectTransform>().rect.width;
+        _rightEdge = width - 2;
+        _leftEdge = -width;
+    }
+
+    private void Update()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x + (_scrollSpeed * Time.deltaTime), transform.localPosition.y, transform.localPosition.z);
+
+        if (transform.localPosition.x <= _leftEdge)
+        {
+            transform.localPosition = new Vector3(_rightEdge, transform.localPosition.y, transform.localPosition.z);
+        }
+    }
+}
